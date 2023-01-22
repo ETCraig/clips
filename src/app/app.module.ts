2160edcd794e009f1c1f294b79447deaa513556c
Craig <dev.etcraig@gmail.com>
2023-01-22 13:55:00 -0700

    Expanded Limits on budgets - Created Clip-List & fb-timestamp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,11 @@ import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { ClipListComponent } from './clip-list/clip-list.component';
+import { FbTimestampPipe } from './pipes/fb-timestamp.pipe';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,8 @@ import { AngularFireStorageModule } from '@angular/fire/compat/storage';
     AboutComponent,
     ClipComponent,
     NotFoundComponent,
+    ClipListComponent,
+    FbTimestampPipe,
   ],
   imports: [
     BrowserModule,
@@ -31,7 +34,6 @@ import { AngularFireStorageModule } from '@angular/fire/compat/storage';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    VideoModule,
     AppRoutingModule,
     AngularFireStorageModule,
   ],
